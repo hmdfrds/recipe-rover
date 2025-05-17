ff9bf415e3e806fa2862e3bcdd117ea72cf49c93
hmdfrds <firdaus1337@gmail.com>
2025-05-18 04:20:05 +0800

    implement uesr login with jwt and auth middleware

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,16 @@
 import pool from "../config/db.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
+
+// @desc    Register a new user
+// @desc    POST /api/users/register
+// @access  Public
 const registerUser = async (req, res) => {
   const { username, email, password } = req.body;
 
@@ -41,7 +51,11 @@ const registerUser = async (req, res) => {
     const password_hash = await bcrypt.hash(password, salt);
 
     const newUserQuery = `INSERT INTO Users (username, email, password_hash) VALUES ($1, $2, $3) RETURNING user_id, username, email, created_at;`;
-    const newUser = await pool.query(newUserQuery, [username, email, password]);
+    const newUser = await pool.query(newUserQuery, [
+      username,
+      email,
+      password_hash,
+    ]);
 
     if (newUser.rows.length > 0) {
       const user = newUser.rows[0];
@@ -73,4 +87,48 @@ const registerUser = async (req, res) => {
   }
 };
 
-export { registerUser };
+// @desc    Authenticate user & get token (Login)
+// @route   POST /api/users/login
+// @access  Public
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Please provide both email and password." });
+  }
+
+  try {
+    const userQuery = "SELECT * FROM Users WHERE email = $1";
+    const { rows } = await pool.query(userQuery, [email]);
+
+    if (rows.length === 0) {
+      return res.status(401).json({ message: "Invalid email or password." });
+    }
+
+    const user = rows[0];
+
+    console.log(user.password_hash);
+    const isMatch = await bcrypt.compare(password, user.password_hash);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid email or password." });
+    }
+
+    const token = generateToken(user.user_id);
+
+    res.status(200).json({
+      user_id: user.user_id,
+      username: user.username,
+      email: user.email,
+      token: token,
+      message: "User logged in successfully.",
+    });
+  } catch (error) {
+    console.error("Error during user login:", error);
+    res.status(500).json({ message: "Server error during user login." });
+  }
+};
+
+export { registerUser, loginUser, generateToken };
---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,51 @@
+import jwt from "jsonwebtoken";
+import pool from "../config/db.js";
+
+const protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      const userQuery = await pool.query(
+        "SELECT user_id, username, email, created_at FROM Users WHERE user_id = $1;",
+        [decoded.userId]
+      );
+
+      if (userQuery.rows.length === 0) {
+        return res
+          .status(401)
+          .json({ message: "Not authorized, user not found." });
+      }
+
+      req.user = userQuery.rows[0];
+      next();
+    } catch (error) {
+      console.error("Token verification failed:", error.message);
+      if (error.name === "JsonWebTokenError") {
+        return res.status(401).json({
+          message:
+            "Not authorized, token failed (invalid signature or malformed).",
+        });
+      }
+      if (error.name === "TokenExpiredError") {
+        return res
+          .status(401)
+          .json({ message: "Not authorized, token expired." });
+      }
+      return res.status(401).json({ message: "Not authorized, token failed." });
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: "Not authorized, no token provided." });
+  }
+};
+
+export { protect };
